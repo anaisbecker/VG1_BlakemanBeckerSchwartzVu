a1ef2ef729bac768bb373200a9a357944e78fbe2
anaisbecker <a.becker@wustl.edu>
2023-09-14 09:59:37 -0500

    added jump, rigidbody code

--- Assets/Code/Adventurer.cs
+++ Assets/Code/Adventurer.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Adventurer : MonoBehaviour
 {
+    // Outlet
+
+    Rigidbody2D _rigidbody2D;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.velocity = transform.right * 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // walk backward
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-0.2f, 0, 0);
         }
 
+        // walk forward
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(0.2f, 0, 0);
+        }
+
+        // jump
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
         }
     }
 }
