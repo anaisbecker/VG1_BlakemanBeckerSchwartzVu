9fec26a58380b371081f35d72fbe302e4182082a
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2023-09-13 19:13:52 -0500

    side to side movement added

--- Assets/Code/Adventurer.cs
+++ Assets/Code/Adventurer.cs
@@ -13,6 +13,14 @@ public class Adventurer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.2f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.2f, 0, 0);
+        }
     }
 }
