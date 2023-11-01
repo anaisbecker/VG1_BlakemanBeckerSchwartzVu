750047baa3b5f62127b0a96ef7a0f00359194740
Tram Vu <82486188+ngoctramvu@users.noreply.github.com>
2023-10-31 21:58:48 -0500

    Add Scoreboard panel with placeholder values

--- Assets/Code/Menu.cs
+++ Assets/Code/Menu.cs
@@ -2,9 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
+    public static Menu instance;
+
+    // Outlets
+    public GameObject scoreMenu;
+    public TMP_Text textLevel;
+    public TMP_Text[] textRankedTimes;
+
+    void Awake()
+    {
+        instance = this;
+        HideScoreMenu();
+    }
+
+    public void ShowScoreLevel1()
+    {
+        textLevel.text = "Level 1";
+    }
+
+    public void ShowScoreLevel2()
+    {
+        textLevel.text = "Level 2";
+    }
+
+    public void ShowScoreMenu()
+    {
+        ShowScoreLevel1();
+        scoreMenu.SetActive(true);
+    }
+
+    public void HideScoreMenu()
+    {
+        scoreMenu.SetActive(false);
+    }
+
+
     public void OnPlayLevel1()
     {
         SceneManager.LoadScene(1);
